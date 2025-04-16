9d49eca499ff353d3fd9079b68932660845919ed
Julia Mammen <jmammen1@cs.swarthmore.edu>
2025-04-16 16:41:35 -0400

    comments

--- static/post.js
+++ static/post.js
@@ -7,4 +7,7 @@ let up_votes = document.getElementById("up_votes")
 pic_votes.onclick = function () {
     clickedCount++;
     up_votes.innerHTML = clickedCount;
+    console.log(clickedCount)
 }
+
+
